b1b49a5cab48664b9b99bdf70c3a621594bb666a
neyonse <neyonse@gmail.com>
2023-06-30 13:19:07 +0300

    small update

--- src/js/fetch-api.js
+++ src/js/fetch-api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const BASE_URL = 'https://books-backend.p.goit.global/books';
-// const MY_API_KEY = '37383891-385c0c3fa5b4b213da48ba87c';
 
 export async function fetchCategoriesList() {
   const url = `${BASE_URL}/category-list`;
@@ -23,23 +22,3 @@ export async function fetchCategory(selectedCategoryName) {
   const response = await axios.get(url);
   return response.data;
 }
-
-// function getFetchCategoriesUrl(inputData, page) {
-//   const q = inputData.replace(' ', '+');
-
-//   const params = {
-//     key: MY_API_KEY,
-//     q,
-//     image_type: 'photo',
-//     orientation: 'horizontal',
-//     safesearch: true,
-//     page,
-//     per_page: 40,
-//   };
-
-//   const paramString = Object.entries(params)
-//     .map(([key, value]) => `${key}=${value}`)
-//     .join('&');
-
-//   return `${BASE_URL}?${paramString}`;
-// }
